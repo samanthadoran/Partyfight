2d520f5f4ef29f7e7a1a654fbe717f4b102196de
Samantha Doran <samanthadoran3@gmail.com>
2014-09-28 06:33:34 -0400

    Add a work around for checking to see what key as been hit, consider making an input vector

--- src/manualcontroller.cpp
+++ src/manualcontroller.cpp
@@ -8,7 +8,15 @@ ManualController::ManualController() : Controller()
 //This needs to catch an event somehow?
 void ManualController::doControls()
 {
-
+    for(int i = 0; i < sf::Keyboard::KeyCount; ++i)
+    {
+        sf::Keyboard::Key k = static_cast<sf::Keyboard::Key>(i);
+        if(sf::Keyboard::isKeyPressed(k))
+        {
+            currentControl = k;
+            break;
+        }
+    }
 }
 
 ManualController::~ManualController()
